8a5e43a02bfe84e0a38e2f6524431801dfe8f7c2
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-04-11 09:54:44 +0900

    chore: 주석 생성

--- Projects/App/Sources/ContentView.swift
+++ Projects/App/Sources/ContentView.swift
@@ -9,6 +9,7 @@ public struct ContentView: View {
   public var body: some View {
     Text("Hello, susu!")
       .padding()
+    //TODO: Delete plz...
     Color(DesignSystemColor.systemBlue)
   }
 }
--- Projects/Share/Designsystem/Sources/KeepSources.swift
+++ Projects/Share/Designsystem/Sources/KeepSources.swift
@@ -1,6 +1,8 @@
 // Content of KeepResources.swift
 import UIKit
 
+
 public enum DesignSystemColor {
+  //TODO: Delte plz
   public static let systemBlue: UIColor = .blue
 }
