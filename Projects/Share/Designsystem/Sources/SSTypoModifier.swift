e036c17fcc889c008d15d670b310e5caefd1a4a7
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-04-17 12:45:00 +0900

    feat: Typo Text 추가
    rename, SSTextModifier -> SSTypoModifier
    add Typo Text

--- Projects/Share/Designsystem/Sources/SSTypoModifier.swift
+++ Projects/Share/Designsystem/Sources/SSTypoModifier.swift
@@ -32,6 +32,8 @@ public struct SSText: View {
 public struct SSTextModifier: ViewModifier {
   private let designSystemFont: SSFont
   private let isBold: Bool
+  
+  @available(*, deprecated, renamed: "SSTypoModifier", message: "SSTextModifier는 SSFont에 Text가 추가됨에 따라, isBold 가 init에서 삭제된 SSTypoModifier를 를 이용해주세요")
   public init(_ designSystemFont: SSFont, isBold: Bool = false) {
     self.designSystemFont = designSystemFont
     self.isBold = isBold
@@ -46,3 +48,20 @@ public struct SSTextModifier: ViewModifier {
       .lineSpacing(designSystemFont.lineHeight)
   }
 }
+
+public struct SSTypoModifier: ViewModifier {
+  private let designSystemFont: SSFont
+  public init(_ designSystemFont: SSFont) {
+    self.designSystemFont = designSystemFont
+  }
+
+  @ViewBuilder
+  public func body(content: Content) -> some View {
+    content
+      .font(designSystemFont.font)
+      .tracking(ssLetterSpacing)
+      .lineSpacing(designSystemFont.lineHeight)
+  }
+}
+
+
