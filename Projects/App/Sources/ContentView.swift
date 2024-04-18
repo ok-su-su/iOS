06966000101571f6fa7a0abdd535e258fe431a0b
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-04-18 21:18:05 +0900

    [Feature] DesignSystem Color Assets 추가(#12)
    [Feature] DesignSystem Color Assets 추가

--- Projects/App/Sources/ContentView.swift
+++ Projects/App/Sources/ContentView.swift
@@ -21,82 +21,16 @@ public struct ContentView: View {
         .padding()
         .foregroundStyle(Color(SSColor.blue50))
 
-      HStack {
-        SSButton(.init(size: .lh46, status: .active, style: .filled, color: .black, buttonText: "Button"), onTap: {})
-        SSButton(
-          .init(
-            size: .lh54, status: .active, style: .filled, color: .black,
-            leftIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            rightIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            buttonText: "Button"
-          ), onTap: {}
-        )
-
-        SSButton(
-          .init(
-            size: .lh54, status: .inactive, style: .lined, color: .black,
-            leftIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            rightIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            buttonText: "Button"
-          ), onTap: {}
-        )
-      }
-
-      HStack {
-        SSButton(.init(size: .lh46, status: .inactive, style: .filled, color: .orange, buttonText: "Button"), onTap: {})
-        SSButton(
-          .init(
-            size: .lh54, status: .active, style: .filled, color: .orange,
-            leftIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            rightIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            buttonText: "Button"
-          ), onTap: {}
-        )
-
-        SSButton(
-          .init(
-            size: .lh54, status: .active, style: .lined, color: .orange,
-            leftIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            rightIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            buttonText: "Button"
-          ), onTap: {}
-        )
-      }
-
-      HStack {
-        SSButton(.init(size: .lh46, status: .inactive, style: .filled, color: .orange, buttonText: "Button"), onTap: {})
-        SSButton(
-          .init(
-            size: .lh54, status: .active, style: .filled, color: .orange,
-            leftIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            rightIcon: .icon(.init(uiImage: SSImage.voteMainFill)),
-            buttonText: "Button"
-          ), onTap: {}
-        )
-
-        SSButton(
-          .init(
-            size: .lh46, status: .inactive, style: .lined, color: .orange,
-            buttonText: "tton"
-          ), onTap: {}
-        )
-      }
-
-      Color(SSColor.gray15)
+  public var body: some View {
+    VStack {
+      
+      Text("Hello, susu!")
+        .padding()
+        .foregroundStyle(Color(SSColor.blue50))
+      
+      Color(SSColor.blue100)
         .frame(width: 100, height: 100)
         .padding()
-
-      Image(uiImage: SSImage.commonLogo)
-        .frame(width: 400, height: 400, alignment: .center)
-    }
-    .customAlert(
-      isPresented: $showingSheet,
-      messageAlertProperty:
-      .init(titleText: "asdf", contentText: "asdf", checkBoxMessage: .none, buttonMessage: .singleButton("asdf"), didTapCompletionButton: {})
-    )
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background {
-      SSColor.gray30
     }
   }
 }
