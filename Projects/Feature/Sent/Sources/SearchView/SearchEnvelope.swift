63105bd2b21a8770310a2385ea0fff96f13b735f
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-05-01 18:16:48 +0900

    feat: swiftlint 적용

--- Projects/Feature/Sent/Sources/SearchView/SearchEnvelope.swift
+++ Projects/Feature/Sent/Sources/SearchView/SearchEnvelope.swift
@@ -9,6 +9,8 @@ import ComposableArchitecture
 import Designsystem
 import Foundation
 
+// MARK: - SearchEnvelope
+
 @Reducer
 struct SearchEnvelope {
   @ObservableState
@@ -73,15 +75,17 @@ struct SearchEnvelope {
   }
 }
 
+// MARK: - SearchViewAdaptor
 
 struct SearchViewAdaptor {
-  init () {}
-  init (sentPeople: [SentPerson]) {
+  init() {}
+  init(sentPeople: [SentPerson]) {
     self.sentPeople = sentPeople
   }
-  var sentPeople: [SentPerson] = [.init(name: "김그남"), .init(name:"김그자"), .init(name: "김사랑")]
+
+  var sentPeople: [SentPerson] = [.init(name: "김그남"), .init(name: "김그자"), .init(name: "김사랑")]
   var latestSearch: [String] = ["김그남", "김그자", "김사랑"]
-  
+
   func filterByTextField(_ textFieldText: String) -> [SentPerson] {
     guard let regex: Regex = try? .init("[\\w\\p{L}]*\(textFieldText)[\\w\\p{L}]*") else {
       return []
