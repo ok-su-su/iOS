ba709ccb12b347181e805a72b3fa78fd84317843
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-05-05 23:03:36 +0900

    feat: Publisher 생성

---
+++ Projects/Feature/Sent/Sources/CreateEnvelopeRouter/CreateEnvelopeRouterProgressPublisher.swift
@@ -0,0 +1,15 @@
+//
+//  CreateEnvelopeRouterProgressPublisher.swift
+//  Sent
+//
+//  Created by MaraMincho on 5/5/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class CreateEnvelopeRouterProgressPublisher {
+  private var progressValue = PassthroughSubject<Int, Never>()
+
+}
