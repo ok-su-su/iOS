876dc805f4f2e3b4d7c58eace11bd00c9ec4d6c9
MaraMincho <seemaster300@naver.com>
2024-05-12 11:34:23 +0900

    feat: SwiftLint 적용

--- Projects/Feature/MyPage/Sources/MyPageMain/ProfileMainRouter.swift
+++ Projects/Feature/MyPage/Sources/MyPageMain/ProfileMainRouter.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 final class MyPageMainRouter: UIHostingController<MyPageMainView> {
   var subscription: AnyCancellable? = nil
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
