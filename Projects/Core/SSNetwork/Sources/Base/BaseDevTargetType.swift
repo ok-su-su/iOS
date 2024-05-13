0728eae8580c5fab8408a7ff731ea237b487928c
Zizi_Kim <rlarjsdn3@naver.com>
2024-05-13 13:04:31 +0900

    feat: BaseTargetType 구현 (#82)

---
+++ Projects/Core/SSNetwork/Sources/Base/BaseDevTargetType.swift
@@ -0,0 +1,31 @@
+//
+//  BaseTestTargetType.swift
+//  SSNetwork
+//
+//  Created by 김건우 on 5/13/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+// MARK: - Protocol
+protocol BaseDevTargetType: TargetType { }
+
+// MARK: - Extensions
+extension BaseDevTargetType {
+    
+    var baseURL: URL {
+        URL(string: "https://api.oksusu.site/api/v1/dev")!
+    }
+    
+    var headers: [String : String]? {
+        ["Content-Type": "json/application"]
+    }
+    
+    var validationType: ValidationType {
+        return .successAndRedirectCodes
+    }
+    
+}
---
+++ Projects/Core/SSNetwork/Sources/Base/BaseTargetType.swift
@@ -0,0 +1,31 @@
+//
+//  BaseTargetType.swift
+//  SSNetwork
+//
+//  Created by 김건우 on 5/13/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+// MARK: - Protocol
+protocol BaseTargetType: TargetType { }
+
+// MARK: - Extensions
+extension BaseTargetType {
+    
+    var baseURL: URL {
+        URL(string: "https://api.oksusu.site/api/v1")!
+    }
+    
+    var headers: [String : String]? {
+        ["Content-Type": "json/application"]
+    }
+    
+    var validationType: ValidationType {
+        return .successAndRedirectCodes
+    }
+    
+}
