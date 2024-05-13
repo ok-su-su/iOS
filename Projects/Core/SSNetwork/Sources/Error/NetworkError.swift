6fee74601dc538c29eddfd35058f6d069091da2a
Zizi_Kim <rlarjsdn3@naver.com>
2024-05-13 12:04:46 +0900

    feat: NetworkError 구현 (#82)

---
+++ Projects/Core/SSNetwork/Sources/Error/NetworkError.swift
@@ -0,0 +1,33 @@
+//
+//  SSError.swift
+//  SSNetwork
+//
+//  Created by 김건우 on 5/13/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Error
+enum NetworkError: Error {
+    case accessTokenExpired
+    case refreshTokenExpired
+    case jsonDecodingFailure
+    case unknown
+}
+
+// MARK: - Extensions
+extension NetworkError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .accessTokenExpired:
+            return "ERROR: Access Token Expired"
+        case .refreshTokenExpired:
+            return "ERROR: Refresh Token Expired"
+        case .jsonDecodingFailure:
+            return "ERROR: Json Decoding Failure"
+        case .unknown:
+            return "ERROR: Unknown"
+        }
+    }
+}
