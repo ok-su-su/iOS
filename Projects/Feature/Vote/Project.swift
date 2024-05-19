34567f91ad86333db49386817178eb9713684b1e
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-05-19 15:43:37 +0900

    feat: Vote Module 생성

---
+++ Projects/Feature/Vote/Project.swift
@@ -0,0 +1,17 @@
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+  name: "Vote",
+  targets: .feature(
+    .vote,
+    testingOptions: [.unitTest],
+    dependencies: [
+      .core(.coreLayers),
+      .share(.designsystem),
+      .share(.sSAlert)
+    ],
+    testDependencies: []
+  )
+)
