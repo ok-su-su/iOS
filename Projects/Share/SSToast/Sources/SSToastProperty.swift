d3f10c4ad80585757580fef0426c25406d37018c
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-05-20 22:44:24 +0900

    [Feature] SSToast 생성 (#103)
    * proj: SSToast추가
    
    * feat: ToastView 구현
    
    * feat: toastView Logic 컴포넌트 구현
    
    * proj: Toast Preview 생성
    
    * feat: TestCase 작성
    
    * docs: 주석 제거
    
    * bug: UnitTest -> UITest로 product Type 변경
    
    * chore: test naming 변경

---
+++ Projects/Share/SSToast/Sources/SSToastProperty.swift
@@ -0,0 +1,36 @@
+//
+//  SSToastProperty.swift
+//  SSToast
+//
+//  Created by MaraMincho on 5/18/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+public struct SSToastProperty: Equatable {
+  var toastMessage: String
+  var type: TrailingType
+  var duration: Double
+
+  public enum TrailingType: Equatable {
+    case none
+    case text(String)
+    case refresh
+  }
+
+  /// ToastMessage에 필요한 Type을 정의합니다.
+  /// - Parameters:
+  ///   - toastMessage: Toast에 표시할 메시지입니다.
+  ///   - type: Toast 오른쪽에 나타날 버튼에 대한 타입을 상정합니다.
+  ///   - duration: Toast지속시간을 나타냅니다.
+  public init(toastMessage: String, trailingType type: TrailingType, duration: Double) {
+    self.toastMessage = toastMessage
+    self.type = type
+    self.duration = duration
+  }
+
+  mutating func setToastMessage(_ val: String) {
+    toastMessage = val
+  }
+}
