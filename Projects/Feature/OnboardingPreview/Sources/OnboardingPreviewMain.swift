194a616b6808941a02f98977a3b02962d6002cc3
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-06 19:54:33 +0900

    feat: Onboarding BuilderView 생성

---
+++ Projects/Feature/OnboardingPreview/Sources/OnboardingPreviewMain.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingPreviewMain.swift
+//  OnboardingPreview
+//
+//  Created by MaraMincho on 6/6/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Onboarding
+import SwiftUI
+
+@main
+struct OnboardingPreviewMain: App {
+  var body: some Scene {
+    WindowGroup {
+      OnboardingBuilderView()
+    }
+  }
+}
