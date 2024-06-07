f0f114dc55105af9430c2b4dd1468f69ce412e1c
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-07 15:03:14 +0900

    feat: 주석 작성

---
+++ Projects/Share/Designsystem/Sources/SSTextFieldCommonRegex.swift
@@ -0,0 +1,13 @@
+//
+//  SSTextFieldCommonRegex.swift
+//  Designsystem
+//
+//  Created by MaraMincho on 6/7/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+public enum SSTextFieldCommonRegex {
+  static let name = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]{1,10}$"
+}
--- Projects/Share/Designsystem/Sources/SSTextFieldWithTCA.swift
+++ Projects/Share/Designsystem/Sources/SSTextFieldWithTCA.swift
@@ -6,49 +6,89 @@
 //  Copyright © 2024 com.oksusu. All rights reserved.
 //
 
-import SwiftUI
 import ComposableArchitecture
+import OSLog
+import SwiftUI
 
-
+// MARK: - SSTextFieldReducerProperty
 
 public struct SSTextFieldReducerProperty: Equatable {
   var text: String
+  var maximumTextLength: Int
+  var regexPattern: String
   var placeHolderText: SSTextPlaceholderText
   var isFocus: Bool = false
   var status: TextFieldStatus
   var errorMessage: String
-  
+
   var textLength: Int {
     return text.count
   }
-  
+
   var lineStatus: LineStatus {
     if status == .error {
       return .red
-    }else if status == .active && text.isEmpty {
+    } else if status == .active && text.isEmpty {
       return .gray
     }
     return .black
   }
-  
-  public init(text: String, placeHolderText: SSTextPlaceholderText, status: TextFieldStatus, errorMessage: String) {
+
+  /// SSTextFieldReducerProperty의 Reducer을 생성하기 위해 필요한 Property입니다.
+  /// - Parameters:
+  ///   - text: 초기 입력되는 TextField의 Text입니다. 이 변수 통해서, Text가 저장됩니다.
+  ///   - maximumTextLength: textField의 최대 입력할 수 있는 글자수입니다.
+  ///   - regexPattern: 정규식을 통해서 에러를 검출하게 되는데, 정규식의 String형태를 입력받습니다.
+  ///   - placeHolderText: PlaceHoloderText의 Tpye을 입력받습니다.
+  ///   - isFocus: Focuse되는지에 대한 변수 입니다.
+  ///   - status: 현재 Status에 대해서 입력받습니다.
+  ///   - errorMessage: Error Status일 때 표시될 에러 메시지 입니다.
+  init(
+    text: String,
+    maximumTextLength: Int,
+    regexPattern: String,
+    placeHolderText: SSTextPlaceholderText,
+    isFocus: Bool = false,
+    status: TextFieldStatus = .active,
+    errorMessage: String
+  ) {
     self.text = text
+    self.maximumTextLength = maximumTextLength
+    self.regexPattern = regexPattern
     self.placeHolderText = placeHolderText
+    self.isFocus = isFocus
     self.status = status
     self.errorMessage = errorMessage
   }
-  
+
   public enum TextFieldStatus {
     case active
     case error
   }
-  
+
   public enum LineStatus {
     case gray
     case black
     case red
   }
+
+  public mutating func changeErrorMessage(text: String) {
+    errorMessage = text
+  }
+
+  private func isValidation(text: String) -> Bool {
+    do {
+      let regex = try NSRegularExpression(pattern: regexPattern, options: .caseInsensitive)
+      let range = NSRange(location: 0, length: text.utf16.count)
+      return regex.firstMatch(in: text, options: [], range: range) != nil
+    } catch {
+      os_log("입력한 정규식이 잘못 되었습니다. \n \(regexPattern)")
+      return false
+    }
+  }
 }
+
+// MARK: - SSTextFieldReducer
 
 @Reducer
 public struct SSTextFieldReducer {
@@ -56,21 +96,22 @@ public struct SSTextFieldReducer {
   public struct State: Equatable {
     @Shared var property: SSTextFieldReducerProperty
     public init(property: Shared<SSTextFieldReducerProperty>) {
-      self._property = property
+      _property = property
     }
   }
+
   public enum Action: Equatable {
     case changeTextField(String)
     case tappedCloseButton
   }
-  
+
   public var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case let .changeTextField(text):
         state.property.text = text
         return .none
-        
+
       case .tappedCloseButton:
         return .send(.changeTextField(""))
       }
@@ -81,12 +122,11 @@ public struct SSTextFieldReducer {
 // MARK: - SSTextFieldView
 
 public struct SSTextFieldView: View {
-  
   @FocusState public var isFocus: Bool
-  
+
   @Bindable
   var store: StoreOf<SSTextFieldReducer>
-  init (store: StoreOf<SSTextFieldReducer>) {
+  public init(store: StoreOf<SSTextFieldReducer>) {
     self.store = store
   }
 
@@ -104,7 +144,10 @@ public struct SSTextFieldView: View {
         .keyboardType(store.property.placeHolderText.keyboardType)
         .frame(maxWidth: .infinity)
         .focused($isFocus)
-        
+        .onChange(of: store.property.isFocus) { _, newValue in
+          isFocus = newValue
+        }
+
         if store.property.text.isEmpty {
           SSImage
             .signupClose
@@ -113,47 +156,34 @@ public struct SSTextFieldView: View {
             }
             .padding(.horizontal, 12)
         }
-        
+
         Text("\(store.property.textLength)/20")
           .foregroundStyle(store.property.status == .active ? SSColor.gray30 : .red60)
       }
       .padding(8)
       .frame(maxWidth: .infinity)
-      
+
       // Line
       makeLine()
         .frame(width: .infinity, height: 1)
-      
+
       Spacer()
         .frame(height: 8)
       // Error Message
-      
+
       if store.property.status == .error {
         Text(store.property.errorMessage)
           .foregroundStyle(SSColor.red60)
       }
     }
   }
-  
+
   @ViewBuilder
-  private func makeLine() -> some View{
+  private func makeLine() -> some View {
     switch store.property.lineStatus {
-      case .red: SSColor.red60
-      case .black: SSColor.gray100
-      case .gray: SSColor.gray50
+    case .red: SSColor.red60
+    case .black: SSColor.gray100
+    case .gray: SSColor.gray50
     }
-  }
-
-  private func isValidation(text: String) -> Bool {
-    let pattern = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]*$"
-    if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
-      let range = NSRange(location: 0, length: text.utf16.count)
-
-      if regex.firstMatch(in: text, options: [], range: range) != nil && text.count <= 10 {
-        return true
-      }
-    }
-
-    return false
   }
 }
