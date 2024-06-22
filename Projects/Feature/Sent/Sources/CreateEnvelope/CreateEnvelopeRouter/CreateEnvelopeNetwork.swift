9a30eebd89ddc4d5746e0c20c20d0c883c7c2260
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-22 23:53:05 +0900

    feat: network 생성

---
+++ Projects/Feature/Sent/Sources/CreateEnvelope/CreateEnvelopeRouter/CreateEnvelopeNetwork.swift
@@ -0,0 +1,16 @@
+//
+//  CreateEnvelopeNetwork.swift
+//  Sent
+//
+//  Created by MaraMincho on 6/22/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+import Dependencies
+import SSNetwork
+import Moya
+
+struct CreateEnvelopeNetwork {
+  
+}
