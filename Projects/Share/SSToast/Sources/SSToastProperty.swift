687e8d120d75e175adb3a3820a9437e2b974c2b6
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-27 08:14:52 +0900

    feat: Toast 함수 수정

--- Projects/Share/SSToast/Sources/SSToastProperty.swift
+++ Projects/Share/SSToast/Sources/SSToastProperty.swift
@@ -24,10 +24,22 @@ public struct SSToastProperty: Equatable {
   ///   - toastMessage: Toast에 표시할 메시지입니다.
   ///   - type: Toast 오른쪽에 나타날 버튼에 대한 타입을 상정합니다.
   ///   - duration: Toast지속시간을 나타냅니다.
+  @available(*, deprecated, renamed: "SSToastProperty(toastMessage:trailingType:)", message: "Toast의 기본 지속시간이 입력된 init함수를 사용해 주세요")
   public init(toastMessage: String, trailingType type: TrailingType, duration: Double) {
     self.toastMessage = toastMessage
     self.type = type
     self.duration = duration
+    
+  }
+  
+  /// ToastMessage에 필요한 Type을 정의합니다.
+  /// - Parameters:
+  ///   - toastMessage: Toast에 표시할 메시지입니다.
+  ///   - type: Toast 오른쪽에 나타날 버튼에 대한 타입을 상정합니다.
+  public init(toastMessage: String, trailingType type: TrailingType) {
+    self.toastMessage = toastMessage
+    self.type = type
+    duration = 2.75
   }
 
   mutating func setToastMessage(_ val: String) {
