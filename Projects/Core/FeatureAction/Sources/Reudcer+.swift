5fe5e9bb201d755f0da33f06ee3af963d8aae86f
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-29 23:40:08 +0900

    feat: add Reducer Extenstion

---
+++ Projects/Core/FeatureAction/Sources/Reudcer+.swift
@@ -0,0 +1,73 @@
+//
+//  Reudcer+.swift
+//  FeatureAction
+//
+//  Created by MaraMincho on 6/29/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+import ComposableArchitecture
+
+// MARK: - Recommended Use Like This
+
+// var body: some Reducer<State, Action> {
+//  Reduce { state, action in
+//    switch action {
+//    case let .view(currentAction):
+//      return viewAction(&state, currentAction)
+//    case let .inner(currentAction):
+//      return innerAction(&state, currentAction)
+//    case let .async(currentAction):
+//      return asyncAction(&state, currentAction)
+//    case let .scope(currentAction):
+//      return scopeAction(&state, currentAction)
+//    case let .delegate(currentAction):
+//      return delegateAction(&state, currentAction)
+//    }
+//  }
+// }
+// var viewAction: (_ state: inout State, _ action: Action.ViewAction) -> Effect<Action> = { state, action in
+//  switch action {
+//  case let .onAppear(isAppear):
+//    if state.isOnAppear {
+//      return .none
+//    }
+//    state.isOnAppear = isAppear
+//    return .none
+//  }
+// }
+// var scopeAction: (_ state: inout State, _ action: Action.ScopeAction) -> Effect<Action> = { _, _ in
+//  return .none
+// }
+// var innerAction: (_ state: inout State, _ action: Action.InnerAction) -> Effect<Action> = { _, _ in
+//  return .none
+// }
+// var asyncAction: (_ state: inout State, _ action: Action.AsyncAction) -> Effect<Action> = { _, _ in
+//  return .none
+// }
+// var delegateAction: (_ state: inout State, _ action: Action.DelegateAction) -> Effect<Action> = { _, _ in
+//  return .none
+// }
+
+
+public protocol FeatureViewAction: Reducer where Action: FeatureAction {
+  var viewAction: (_ state: inout State, _ action: Action.ViewAction) -> Effect<Action> { get set }
+}
+
+public protocol FeatureScopeAction: Reducer where Action: FeatureAction {
+  var scopeAction: (_ state: inout State, _ action: Action.ScopeAction) -> Effect<Action> { get set }
+}
+
+public protocol FeatureAsyncAction: Reducer where Action: FeatureAction {
+  var asyncAction: (_ state: inout State, _ action: Action.AsyncAction) -> Effect<Action> { get set
+   }
+}
+
+public protocol FeatureInnerAction: Reducer where Action: FeatureAction {
+  var innerAction: (_ state: inout State, _ action: Action.InnerAction) -> Effect<Action> { get set}
+}
+
+public protocol FeatureDelegateAction : Reducer where Action: FeatureAction {
+  var delegateAction: (_ state: inout State, _ action: Action.DelegateAction) -> Effect<Action> { get set}
+}
