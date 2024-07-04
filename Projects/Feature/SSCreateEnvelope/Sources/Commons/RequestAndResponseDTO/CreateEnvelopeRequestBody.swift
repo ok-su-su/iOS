4fbed4f8647761922de60fcdb579a459b410e565
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-07-04 15:55:22 +0900

    feat: request body 수정

--- Projects/Feature/SSCreateEnvelope/Sources/Commons/RequestAndResponseDTO/CreateEnvelopeRequestBody.swift
+++ Projects/Feature/SSCreateEnvelope/Sources/Commons/RequestAndResponseDTO/CreateEnvelopeRequestBody.swift
@@ -11,25 +11,25 @@ import OSLog
 
 // MARK: - CreateEnvelopeRequestBody
 
-struct CreateEnvelopeRequestBody: Codable, Equatable {
+public struct CreateEnvelopeRequestBody: Codable, Equatable {
   /// 봉투의 SENT혹은 RECIVE를 통해 타입을 전달합니다.
-  var type: String
+  public var type: String
   /// 봉투의 친구 ID입니다.
-  var friendID: Int64? = nil
+  public var friendID: Int64? = nil
   /// NIL
-  var ledgerID: Int64? = nil
+  public var ledgerID: Int64? = nil
   /// 봉투의 총액입니다.
-  var amount: Int64? = nil
+  public var amount: Int64? = nil
   /// 어떤 선물을 주고받았는지 활용됩니다.
-  var gift: String? = nil
+  public var gift: String? = nil
   /// 메모 입니다.
-  var memo: String? = nil
+  public var memo: String? = nil
   /// 방문 여부를 나타냅니다.
-  var hasVisited: Bool? = nil
+  public var hasVisited: Bool? = nil
   /// 건넨 날짜를 입력받습니다.
-  var handedOverAt: String? = nil
+  public var handedOverAt: String? = nil
   /// 경조사에 대해서 나타냅니다.
-  var category: CategoryRequestBody?
+  public var category: CategoryRequestBody?
 
   enum CodingKeys: String, CodingKey {
     case type
@@ -57,7 +57,7 @@ extension CreateEnvelopeRequestBody {
 
 // MARK: - CategoryRequestBody
 
-struct CategoryRequestBody: Codable, Equatable {
+public struct CategoryRequestBody: Codable, Equatable {
   var id: Int?
   var customCategory: String?
 
