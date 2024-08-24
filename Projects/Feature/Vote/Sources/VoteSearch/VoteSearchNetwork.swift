883bae5f4395dfdcf91a32a81a4f0bf5ec8f1d10
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-08-24 23:26:54 +0900

    feat: voteSearch Network 작성

--- Projects/Feature/Vote/Sources/VoteSearch/VoteSearchNetwork.swift
+++ Projects/Feature/Vote/Sources/VoteSearch/VoteSearchNetwork.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import Dependencies
+import Moya
+import SSNetwork
+import SSInterceptor
+
+struct VoteSearchNetwork {
+  var searchByVoteName: @Sendable (_ text: String) async throws -> [VoteSearchItem]
+  @Sendable static func _searchByVoteName(_ name: String) async throws -> [VoteSearchItem] {
+    let response: SliceResponseDtoVoteAndOptionsWithCountResponse = try await provider.request(.searchByVoteName(name))
+    return response.data.map{$0.convertToVoteSearchItem()}
+  }
+}
+
+extension VoteSearchNetwork: DependencyKey {
+  static var liveValue: VoteSearchNetwork = .init(searchByVoteName: _searchByVoteName)
+  static var provider = MoyaProvider<Network>(session: .init(interceptor: SSTokenInterceptor.shared))
+
+  enum Network: SSNetworkTargetType {
+    case searchByVoteName(String)
+
+    var additionalHeader: [String : String]? { nil }
+    var path: String { "votes"}
+    var method: Moya.Method {
+      switch self {
+      case .searchByVoteName:
+          .get
+      }
+    }
+    var task: Moya.Task {
+      switch self {
+      case let .searchByVoteName(name):
+          .requestParameters(parameters: ["content": name], encoding: URLEncoding.queryString)
+      }
+    }
+  }
+}
+
+fileprivate extension VoteAndOptionsWithCountResponse {
+  func convertToVoteSearchItem() -> VoteSearchItem {
+    .init(id: id, title: content, firstContentDescription: nil, secondContentDescription: nil)
+  }
+}
