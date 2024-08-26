0fe74061ead8c91c87e1c74c72e087fffae0cd8a
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-08-26 09:05:16 +0900

    feat: version update 1.0.4, build version update

---
+++ Projects/Core/CommonExtension/Sources/Date+.swift
@@ -0,0 +1,17 @@
+//
+//  String+.swift
+//  CommonExtension
+//
+//  Created by MaraMincho on 8/25/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+  func toDate() -> Date {
+    let dateFormatter: ISO8601DateFormatter = .init()
+
+  }
+}
+
---
+++ Projects/Feature/Vote/Sources/Commons/Extension/Date+.swift
@@ -0,0 +1,9 @@
+//
+//  Date+.swift
+//  Vote
+//
+//  Created by MaraMincho on 8/25/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
