c995e89264b4622481df0b028742d9786d57a740
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-08-27 13:19:49 +0900

    feat: creatVoteRequst Function 작성

---
+++ Projects/Core/SSNetwork/Sources/DataModel/CreateAndUpdateVoteResponse.swift
@@ -0,0 +1,39 @@
+//
+//  CreateAndUpdateVoteResponse.swift
+//  SSNetwork
+//
+//  Created by MaraMincho on 8/27/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+public struct CreateAndUpdateVoteResponse:  Equatable, Decodable  {
+  /// 투표 id
+  public let id: Int64
+  /// 본인 소유 글 여부 / 내 글 : true, 전체 글 : false
+  public let isMine: Bool
+  /// 투표 생성자 id
+  public let uid: Int64
+  /// 보드
+  public let board: BoardModel
+  /// 내용
+  public let content: String
+  /// 수정 여부 / 수정함 : true, 수정 안함 : false
+  public let isModified: Bool
+  /// 투표 옵션
+  public let options: [VoteOptionAndHistoryModel]
+  /// 투표 생성일
+  public let createdAt: Date
+
+  enum CodingKeys: CodingKey {
+    case id
+    case isMine
+    case uid
+    case board
+    case content
+    case isModified
+    case options
+    case createdAt
+  }
+}
--- Projects/Feature/Vote/Sources/WriteVote/WriteVoteNetwork.swift
+++ Projects/Feature/Vote/Sources/WriteVote/WriteVoteNetwork.swift
@@ -12,6 +12,43 @@ import SSInterceptor
 import Moya
 import SSNetwork
 
+
+typealias CreateVoteRequestBody = CreateVoteRequest
+
 struct WriteVoteNetwork {
-  
+  var createVote: @Sendable (CreateVoteRequestBody) async throws -> CreateAndUpdateVoteResponse
+  @Sendable
+  private static func _createVote(_ createVoteRequestBody: CreateVoteRequestBody) async throws -> CreateAndUpdateVoteResponse{
+    let data = try JSONEncoder.default.encode(createVoteRequestBody)
+    return try await provider.request(.createVote(CreateVoteRequestBodyToData: data))
+  }
+}
+
+extension WriteVoteNetwork: DependencyKey {
+  static var liveValue: WriteVoteNetwork = .init(createVote: _createVote)
+
+  static let provider = MoyaProvider<Network>(session: .init(interceptor: SSTokenInterceptor.shared))
+  enum Network: SSNetworkTargetType {
+    case createVote(CreateVoteRequestBodyToData: Data)
+
+    var additionalHeader: [String : String]? { nil }
+    var path: String {
+      switch self {
+      case .createVote:
+        "votes/"
+      }
+    }
+    var method: Moya.Method {
+      switch self {
+      case .createVote:
+          .post
+      }
+    }
+    var task: Moya.Task {
+      switch self {
+      case let .createVote(CreateVoteRequestBodyToData):
+          .requestData(CreateVoteRequestBodyToData)
+      }
+    }
+  }
 }
