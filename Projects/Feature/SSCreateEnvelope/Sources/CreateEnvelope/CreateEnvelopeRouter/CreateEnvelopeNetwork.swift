0b3a32798d179bc2f08ef04fe3858d667369c956
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-08-29 23:43:58 +0900

    feat: refactor CreateEnvelopeNetwork

--- Projects/Feature/SSCreateEnvelope/Sources/CreateEnvelope/CreateEnvelopeRouter/CreateEnvelopeNetwork.swift
+++ Projects/Feature/SSCreateEnvelope/Sources/CreateEnvelope/CreateEnvelopeRouter/CreateEnvelopeNetwork.swift
@@ -15,50 +15,46 @@ import SSNetwork
 
 // MARK: - CreateEnvelopeNetwork
 
-struct CreateEnvelopeNetwork: Equatable {
-  static func == (_: CreateEnvelopeNetwork, _: CreateEnvelopeNetwork) -> Bool { true }
-
-  private let provider = MoyaProvider<Network>(session: .init(interceptor: SSTokenInterceptor.shared))
-  func findIDIfExistPrev(name: String, relationID: Int) async throws -> Int64? {
+struct CreateEnvelopeNetwork {
+  @Sendable private static func _findIDIfExistPrev(name: String, relationID: Int) async throws -> Int64? {
     let data: PageResponseDtoSearchFriendResponse = try await provider.request(.findFriend(name))
     // 검색후에 relationID가 똑같은 경우가 있다면 ID를 리턴합니다.
     return data.data.filter { $0.relationship.id == relationID }.first?.friend.id
   }
 
+  var getFriendID: @Sendable (_ bodyProperty: CreateFriendRequestBody) async throws -> Int64
   /// FriendID를 가져옵니다.
-  func getFriendID(_ bodyProperty: CreateFriendRequestBody) async throws -> Int64 {
+  @Sendable private static func _getFriendID(_ bodyProperty: CreateFriendRequestBody) async throws -> Int64 {
     if let name = bodyProperty.name,
        let relationID = bodyProperty.relationshipId,
        // 만약 사용자가 입력한 이름과 관계가 서버에 존재하는경우 이전 아이디를 리턴합니다.
-       let id = try await findIDIfExistPrev(name: name, relationID: relationID) {
+       let id = try await _findIDIfExistPrev(name: name, relationID: relationID) {
       return id
     }
     // 만약 사용자가 입력한 이름과 관계가 서버에 존재하지 않는 경우 새 friendID를 만듭니다.
     return try await createFriend(bodyProperty)
   }
 
-  func createFriend(_ bodyProperty: CreateFriendRequestBody) async throws -> Int64 {
+  @Sendable private static func createFriend(_ bodyProperty: CreateFriendRequestBody) async throws -> Int64 {
     let data: CreateFriendResponseDTO = try await provider.request(.createFriend(bodyProperty))
     os_log("친구 생성에 성공하였습니다. \(#function)")
     return data.id
   }
 
-  func createEnvelope(_ bodyProperty: CreateEnvelopeRequestBody) async throws -> Data {
+  var createEnvelope: @Sendable (_ bodyProperty: CreateEnvelopeRequestBody) async throws -> Data
+  @Sendable private static func _createEnvelope(_ bodyProperty: CreateEnvelopeRequestBody) async throws -> Data {
     return try await provider.requestData(.createEnvelope(bodyProperty))
   }
 }
 
-extension DependencyValues {
-  var createEnvelopeNetwork: CreateEnvelopeNetwork {
-    get { self[CreateEnvelopeNetwork.self] }
-    set { self[CreateEnvelopeNetwork.self] = newValue }
-  }
-}
-
-// MARK: - CreateEnvelopeNetwork + DependencyKey
+// MARK: DependencyKey
 
 extension CreateEnvelopeNetwork: DependencyKey {
-  static var liveValue: CreateEnvelopeNetwork = .init()
+  static var liveValue: CreateEnvelopeNetwork = .init(
+    getFriendID: _getFriendID,
+    createEnvelope: _createEnvelope
+  )
+  private static let provider = MoyaProvider<Network>(session: .init(interceptor: SSTokenInterceptor.shared))
   enum Network: SSNetworkTargetType {
     case createFriend(CreateFriendRequestBody)
     case findFriend(String)
@@ -96,5 +92,12 @@ extension CreateEnvelopeNetwork: DependencyKey {
         return .requestData(bodyProperty.getData())
       }
     }
+  }
+}
+
+extension DependencyValues {
+  var createEnvelopeNetwork: CreateEnvelopeNetwork {
+    get { self[CreateEnvelopeNetwork.self] }
+    set { self[CreateEnvelopeNetwork.self] = newValue }
   }
 }
