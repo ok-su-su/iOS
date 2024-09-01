bb87c8838655306fae477b5c842d4e5b2f7365da
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-09-01 21:06:19 +0900

    feat: add TCA Mutex minimum Duration function

--- Projects/Core/FeatureAction/Sources/TCAMutex.swift
+++ Projects/Core/FeatureAction/Sources/TCAMutex.swift
@@ -7,6 +7,26 @@
 //
 
 import Foundation
+import OSLog
 import SwiftAsyncMutex
 
 public typealias TCAMutex = AsyncMutexManager
+
+public extension TCAMutex {
+  func waitForFinish(minimumTimeDuration: Double = 0.3) async {
+    // Start Point
+    let initTime = Date()
+    await waitForFinish()
+
+    // End Point
+    let elapsedTime = Date().timeIntervalSince(initTime)
+    if elapsedTime < minimumTimeDuration {
+      let remainingTime = minimumTimeDuration - elapsedTime
+      do {
+        try await Task.sleep(nanoseconds: UInt64(remainingTime * 1_000_000_000))
+      } catch {
+        os_log("error occurred\n\(error.localizedDescription)")
+      }
+    }
+  }
+}
