9a6cedf4fc27c199f642cf589215103658099b17
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-09-06 13:18:25 +0900

    feat: update build version

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -27,7 +27,7 @@ let project = Project.makeModule(
       "UILaunchStoryboardName": "LaunchScreen",
       "BGTaskSchedulerPermittedIdentifiers": "com.oksusu.susu.app",
       "CFBundleShortVersionString": "1.0.4",
-      "CFBundleVersion": "2024090325",
+      "CFBundleVersion": "2024090626",
       "UIUserInterfaceStyle": "Light",
       "ITSAppUsesNonExemptEncryption": "No",
       "AppstoreAPPID": "6503701515",
