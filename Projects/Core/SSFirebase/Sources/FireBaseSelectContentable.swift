9f1b71a94605bcd718fcbd1d589a82071e577f21
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-11-04 21:21:06 +0900

    feat: FireBaseSelectContentable 생성,

---
+++ Projects/Core/SSFirebase/Sources/FireBaseSelectContentable.swift
@@ -0,0 +1,14 @@
+//
+//  FireBaseSelectContentable.swift
+//  SSFirebase
+//
+//  Created by MaraMincho on 11/4/24.
+//  Copyright © 2024 com.oksusu. All rights reserved.
+//
+
+import Foundation
+
+public protocol FireBaseSelectContentable {
+  var eventParameters: [String: Any] { get}
+  var eventName: String { get }
+}
